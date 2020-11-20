52265f82185f712c7cc68699a09d58cd3cd0efc4
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-11-20 14:48:41 +0000

    Initial Creation of Spring Project.

---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/WelshRowingProjectApplication.java
@@ -0,0 +1,13 @@
+package nsa.group7.welshrowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelshRowingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelshRowingProjectApplication.class, args);
+    }
+
+}
---
+++ group7-welshRowing/src/test/java/nsa/group7/welshrowing/WelshRowingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nsa.group7.welshrowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelshRowingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
