43767d5605bbb38163006896e0ddd5cd39f67742
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-11-22 19:36:01 +0000

    Created a class that will allow pre existing information about an athlete to be automatically filled in when returning to the form.

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteController.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AthleteController {
+    /**
+     *  Directs the user to the entry form where when logged in, can enter and save their personal information.
+     * @param model - adds to the page model
+     * @return returns the athlete-entry-form html
+     */
 
     @GetMapping("new-athlete")
     public String serveAthleteEntryForm(Model model) {
+        AthleteEntryForm athleteEntryForm = new AthleteEntryForm();
+        model.addAttribute("athlete", athleteEntryForm);
         return "athlete-entry-form";
     }
 }
---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteEntryForm.java
@@ -0,0 +1,72 @@
+package nsa.group7.welshrowing.web;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class AthleteEntryForm {
+    /**
+     * The name of the athlete.
+     */
+    private String name;
+    /**
+     * The gender of the athlete. Will be radio button options on form.
+     */
+    private String gender;
+    /**
+     * The date of birth of the athlete.
+     */
+    private Date dob;
+    /**
+     * The athlete's personal email address.
+     */
+    private String email;
+    /**
+     * The athlete's mobile number.
+     */
+    private String mobileNumber;
+    /**
+     * The athlete's home telephone number.
+     */
+    private String telephoneNumber;
+    /**
+     * The athlete's home address.
+     */
+    private String homeAddress;
+    /**
+     * The athlete's university address if applicable.
+     */
+    private String uniAddress;
+    /**
+     * The name of the school or university the athlete is currently in.
+     */
+    private String placeOfEducation;
+    /**
+     * The name of the parent or guardian of the athlete, if they are under 18 years of age.
+     */
+    private String guardianName;
+    /**
+     * The relationship of the parent or guardian of the athlete, if they are under 18 years of age.
+     */
+    private String relationshipToAthlete;
+    /**
+     * The phone number of the parent or guardian of the athlete, if they are under 18 years of age.
+     */
+    private String guardianContactNumber;
+    /**
+     * The email of the parent or guardian of the athlete, if they are under 18 years of age.
+     */
+    private String guardianEmail;
+    /**
+     * A short string about where the athlete heard about Rowing Wales.
+     * Will be a group of radio buttons with an optional text box if an athlete has heard about Rowing Wales from another place.
+     */
+    private String heardFrom;
+    /**
+     * A boolean that states if a letter of interest has been sent to the athlete.
+     */
+    private Boolean interestLetter;
+}
