7ce8da6c3a86e8cd6a7be4bd2dda5a7564589bf6
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-11-25 16:13:22 +0000

    Added tests to ensure data can be added and called from the test database.

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/jpa/AthleteRepoJPA.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/jpa/AthleteRepoJPA.java
@@ -3,5 +3,12 @@ package nsa.group7.welshrowing.jpa;
 import nsa.group7.welshrowing.domain.Athlete;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AthleteRepoJPA extends JpaRepository<Athlete, Long> {
+    /**
+     *
+     * @param aName the name to find
+     * @return returns the athletes name
+     */
+    Athlete findByName(String aName);
 }
--- group7-welshRowing/src/test/java/nsa/group7/welshrowing/server/AthleteEntryFormTests.java
+++ group7-welshRowing/src/test/java/nsa/group7/welshrowing/server/AthleteEntryFormTests.java
@@ -1,11 +1,9 @@
-package nsa.group7.welshrowing.mvc;
+package nsa.group7.welshrowing.server;
 
-import nsa.group7.welshrowing.web.AthleteController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -16,11 +14,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {AthleteController.class})
-@WebMvcTest
+@SpringBootTest
 public class AthleteEntryFormTests {
+
     @Autowired
     private MockMvc mockMvc;
+
 
     @Test
     public void shouldGetEntryForm() throws Exception {
@@ -29,7 +28,7 @@ public class AthleteEntryFormTests {
                 .perform(get("/new-athlete"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Athlete Information entry form.")))
-                .andExpect(content().string(containsString("Enter your details here")));
+                .andExpect(content().string(containsString("New Athlete")))
+                .andExpect(content().string(containsString("Please enter your information to create an account.")));
     }
 }
