34422686dd8f8f098dbd7af4f6f8426fd57d1a73
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-11-29 19:05:12 +0000

    removed code disabling xss in WebSecurityConfig.java, no longer works with H2 database.

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/security/WebSecurityConfig.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/security/WebSecurityConfig.java
@@ -17,10 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .antMatchers("/h2-console/**").permitAll();
-
-            http.csrf().disable();
-            http.headers().frameOptions().disable();
+                    .antMatchers("/").permitAll();
         }
     }
