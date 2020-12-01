997630576d362d6dde9cdb6deaac53fd7c4ef81b
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-12-01 20:41:16 +0000

    Added entity details for the database in SessionRPE

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/domain/SessionRPE.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/domain/SessionRPE.java
@@ -1,4 +1,28 @@
 package nsa.group7.welshrowing.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "SessionRPE")
 public class SessionRPE {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sessionRPEID;
+
+    @ManyToOne
+    @JoinTable(name = "athlete", joinColumns = @JoinColumn(name = "athleteID"))
+    private Athlete athleteID;
+
+    private Date date;
+    private String typeOfCrossTraining;
+    private int totalTimeMinutes;
+    private String totalDistance;
 }
