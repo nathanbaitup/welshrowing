09e0d2b3e316fcbc7134daaac36c9008d51abd56
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2020-12-01 19:50:32 +0000

    Test created for database printing functionality. Will read each page for the string provided

--- group7-welshRowing/src/test/java/nsa/group7/welshrowing/mvc/coachDashboardTest.java
+++ group7-welshRowing/src/test/java/nsa/group7/welshrowing/mvc/coachDashboardTest.java
@@ -1,36 +1,55 @@
 package nsa.group7.welshrowing.mvc;
 
+
+import nsa.group7.welshrowing.jpa.ApplicantRepoJPA;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 
 @AutoConfigureMockMvc
-@ContextConfiguration
+@TestPropertySource(locations = "classpath:application-prod.properties")
 @SpringBootTest
-@DataJpaTest
 
 public class coachDashboardTest {
     @Autowired
     private MockMvc mockMvc;
 
+
     @Test
     public void coachNameShows() throws Exception {
         this.mockMvc
-                .perform(get("/coachdashboard/6"))
-                .andDo(print())
+               .perform(get("/coachdashboard/1"))
+//                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Testing Person")));
+                .andExpect(content().string(containsString("Nathan Test")));
+    }
+
+    @Test
+    public void coachNameShowsTwo() throws Exception {
+        this.mockMvc
+                .perform(get("/coachdashboard/2"))
+//                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Adam Jones")));
+    }
+
+    @Test
+    public void coachNameShowsThree() throws Exception {
+        this.mockMvc
+                .perform(get("/coachdashboard/3"))
+//                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Oliver Twist")));
     }
 
 }
