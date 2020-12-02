439a7293e5cc96e1478ce95084b51724f91c9a78
c1989132 <holdawayo@cardiff.ac.uk>
2020-12-02 23:20:02 +0000

    added post method to controller and fixed thymeleaf

---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/domain/MorningMonitoringAuditor.java
@@ -0,0 +1,5 @@
+package nsa.group7.welshrowing.domain;
+
+public interface MorningMonitoringAuditor {
+
+}
--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/MorningMonitoringController.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/MorningMonitoringController.java
@@ -1,8 +1,12 @@
 package nsa.group7.welshrowing.web;
 
+import nsa.group7.welshrowing.domain.MorningMonitoring;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -10,11 +14,26 @@ public class MorningMonitoringController {
 
     @GetMapping("/athlete-mmd-form")
     public String serveMorningMonitoringForm(Model model) {
-        model.addAttribute(" ");
+        MorningMonitoringForm morningMonitoringForm = new MorningMonitoringForm(null,null,null,0,0,0,0,0,0);
+        model.addAttribute("morningMonitoringForm",morningMonitoringForm);
         return "athlete-mmd-form";
     }
 
-//    @PostMapping("/athlete-mmd-form")
-//    public String handleMMDForm()
+    @PostMapping("/athlete-mmd-form")
+    public String handleMMDForm(@ModelAttribute("morningMonitoring") MorningMonitoring morningMonitoring, MorningMonitoringForm morningMonitoringForm, BindingResult bindings, Model model){
+        if (bindings.hasErrors()) {
+            System.out.println("Errors:" + bindings.getFieldErrorCount());
+            for (ObjectError oe : bindings.getAllErrors()) {
+                System.out.println(oe);
+            }
+            model.addAttribute("morningMonitoringForm", morningMonitoringForm);
+            return "athlete-mmd-form";
+        } else {
+            //applicant.setPassword(hashPassword(applicantForm.getPassword()));
+            //applicantAuditor.saveApplicant(applicant);
+            System.out.println(morningMonitoringForm.getAthleteID());
+            return "athlete-mmd-form";
+        }
+    }
 
 }
