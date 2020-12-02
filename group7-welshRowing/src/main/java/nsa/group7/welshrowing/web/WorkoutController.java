71219d68c06cf95d6f30a4f888a02973184048b9
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2020-12-02 20:31:33 +0000

    Added @PostMapping method to handle submission to database via thymeleaf

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/WorkoutController.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/WorkoutController.java
@@ -1,13 +1,20 @@
 package nsa.group7.welshrowing.web;
 
 
+import nsa.group7.welshrowing.domain.crossTraining;
 import nsa.group7.welshrowing.domain.crossTrainingAuditor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class WorkoutController {
@@ -21,8 +28,23 @@ public class WorkoutController {
 
     @GetMapping("submit-crosstraining-form/{athleteID}")
     public String submitCrossTrainingForm(@PathVariable Long athleteID, Model model) {
-        crossTrainingSessionForm crossTrainingSessionForm = new crossTrainingSessionForm(athleteID);
+        crossTrainingSessionForm crossTrainingSessionForm = new crossTrainingSessionForm(athleteID,null,null,null,null);
         model.addAttribute("crossTrainingSessionForm", crossTrainingSessionForm);
         return "crossTrainingForm";
+    }
+
+    @PostMapping("submit-crosstraining-form")
+    public String postSubmitCrossTrainingForm(@ModelAttribute("crossTrainingSessionForm") crossTraining crossTraining, @Valid crossTrainingSessionForm crossTrainingSessionForm, BindingResult bindings, Model model) {
+        if (bindings.hasErrors()) {
+            System.out.println("Errors:" + bindings.getFieldErrorCount());
+            for (ObjectError oe: bindings.getAllErrors()){
+                System.out.println(oe);
+            }
+            model.addAttribute("crossTrainingSessionForm", crossTrainingSessionForm);
+            return "submit-crosstraining-form/" + crossTrainingSessionForm.getAthleteID();
+        } else {
+            crossTrainingAuditor.saveCrossTrainingData(crossTraining);
+            return "redirect:/";
+        }
     }
 }
