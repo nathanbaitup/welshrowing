a982376d2c9cb986a35fd21a41743cda90b6ff8c
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-12-03 01:46:08 +0000

    Fixed a bug where athleteID was not stored in the AthleteUpdateForm

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteUpdateForm.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteUpdateForm.java
@@ -15,6 +15,10 @@ import java.util.Date;
 @NoArgsConstructor
 public class AthleteUpdateForm {
     /**
+     * The athleteID from the database.
+     */
+    private Long athleteID;
+    /**
      * The name of the athlete.
      */
     @NotBlank
@@ -92,6 +96,6 @@ public class AthleteUpdateForm {
     private String postTestResult;
 
     public AthleteUpdateForm(Long athleteID, String name){
-        this(athleteID, name, null, null, true,null, null, null, null, null, null, null, null, null, null, null, null, null);
+        this(athleteID, name, null, null, true, null, null, null, null, null, null, null, null, null, null, null, null, null);
     }
 }
