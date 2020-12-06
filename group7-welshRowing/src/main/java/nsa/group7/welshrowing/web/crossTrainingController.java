9d2fdeeb5fac983ee6a01d8daee42a24cec3d11e
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2020-12-06 17:26:46 +0000

    Unversioned files commited

---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/crossTrainingController.java
@@ -0,0 +1,14 @@
+//package nsa.group7.welshrowing.web;
+//
+//import nsa.group7.welshrowing.domain.crossTrainingAuditor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class crossTrainingController {
+//
+//    private final crossTrainingAuditor CrossTrainingAuditor;
+//
+//    @Autowired
+//    public crossTrainingController(crossTrainingAuditor acrossTrainingAuditor) { crossTrainingAuditor=acrossTrainingAuditor; }
+//}
