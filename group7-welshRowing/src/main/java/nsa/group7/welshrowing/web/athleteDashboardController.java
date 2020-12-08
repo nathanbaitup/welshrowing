5172c8505255ff4f08dc43e0cab7cb179ed2a8a5
c1989132 <holdawayo@cardiff.ac.uk>
2020-12-08 13:09:52 +0000

    Added athlete dashboard and controller

---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/athleteDashboardController.java
@@ -0,0 +1,15 @@
+package nsa.group7.welshrowing.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class athleteDashboardController {
+
+    @GetMapping("testgraphs")
+    public String athleteDashboard(Model model){
+        model.addAttribute(" ");
+        return "athleteDashboard";
+    }
+}
