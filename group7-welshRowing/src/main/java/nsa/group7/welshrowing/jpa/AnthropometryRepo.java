2dd7e2ff3b66862220ecd73ec99cfb7cf617ea00
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-12-09 20:21:16 +0000

    Allowed for athletes to access and update medical data if needed.

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/jpa/AnthropometryRepo.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/jpa/AnthropometryRepo.java
@@ -1,7 +1,20 @@
 package nsa.group7.welshrowing.jpa;
 
 import nsa.group7.welshrowing.domain.Anthropometry;
+import nsa.group7.welshrowing.domain.Athlete;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 public interface AnthropometryRepo extends JpaRepository<Anthropometry, Long> {
+    /**
+     * Method that runs the stored procedure to decrypt an athletes medical data if needed to be updated.
+     *
+     * @param id - the athleteID
+     * @param key - the decryption key
+     * @return returns decrypted values to be modified.
+     */
+    @Query(value = "CALL decrypt_data(:theAthleteID, :theKey);", nativeQuery = true)
+    Anthropometry decryptData(@Param("theAthleteID") Long id, @Param("theKey") String key);
 }
