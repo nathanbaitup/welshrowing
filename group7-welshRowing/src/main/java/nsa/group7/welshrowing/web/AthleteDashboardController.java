446f58262afc8e2e7124db0f09a33d5f06e2b157
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-12-15 21:52:32 +0000

    AthleteDashboardController now works with sessions, redirects a user back to their specific dashboard.

---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteDashboardController.java
@@ -0,0 +1,37 @@
+package nsa.group7.welshrowing.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@SessionAttributes(names = {"users"})
+public class AthleteDashboardController {
+    /**
+     *
+     * @return
+     */
+    @ModelAttribute("users")
+    public List<Long> users() {
+        return new ArrayList<Long>();
+    }
+
+    @GetMapping("athlete-dashboard/{id}")
+    public String athleteDashboard(@PathVariable Long id, @ModelAttribute("users") List<Long> users, RedirectAttributes attributes, Model model){
+        if (users.get(users.size() - 1).equals(id)) {
+            System.out.println("List of Users: " + users);
+            model.addAttribute(" ");
+            return "athleteDashboard";
+        } else {
+            return "redirect:/404";
+        }
+
+    }
+}
