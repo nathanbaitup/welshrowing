3627bbff293268f271bc9df5ab90a4f2f5200dbb
c1989132 <holdawayo@cardiff.ac.uk>
2020-12-16 11:27:10 +0000

    added mmd graph form class

---
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteMMDGraphForm.java
@@ -0,0 +1,19 @@
+package nsa.group7.welshrowing.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AthleteMMDGraphForm {
+    //int array for the 7 day waking heart rates
+    private int[] wakingHR;
+
+    //int array for the 7 day waking standing rates
+    private int[] standingHR;
+
+    //int array for the 7 day shape readings
+    private int[] shape;
+}
