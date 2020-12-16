4a998ec2e91a3114deff3df3f425ab1f4b721ddd
c1989132 <holdawayo@cardiff.ac.uk>
2020-12-16 11:59:22 +0000

    graph data is now passed by thymeleaf

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteDashboardController.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteDashboardController.java
@@ -1,5 +1,7 @@
 package nsa.group7.welshrowing.web;
 
+import nsa.group7.welshrowing.domain.MorningMonitoringAuditor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AthleteDashboardController {
+
+    private final MorningMonitoringAuditor graphAuditor;
+
+    @Autowired
+    public AthleteDashboardController(MorningMonitoringAuditor graphAuditor){
+        this.graphAuditor = graphAuditor;
+    }
+
+
     @GetMapping("athlete-dashboard")
     public String athleteDashboard(Model model){
-        model.addAttribute(" ");
+
+        
+
+        int[] wakingHR = {20,20,20,20,20,20};
+        int[] standingHR = {30,30,30,30,30,30};
+        int[] shape = {40,40,40,40,40,40};
+
+        AthleteMMDGraphForm graphData = new AthleteMMDGraphForm(wakingHR, standingHR, shape);
+        model.addAttribute("graphs",graphData);
+
         return "athleteDashboard";
     }
 }
