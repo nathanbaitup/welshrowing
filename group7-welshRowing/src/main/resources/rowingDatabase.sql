ef19990bc8d3e42dc5fe23c5776bf8ad78d69e04
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-12-16 23:12:49 +0000

    Added dummy data with encrypted passwords into database and changed naming to match the web app user.  Fixed the login formatting, and added an update medical data button to the athlete dashboard.

--- group7-welshRowing/src/main/resources/rowingDatabase.sql
+++ group7-welshRowing/src/main/resources/rowingDatabase.sql
@@ -1,11 +1,5 @@
 # https://lucid.app/lucidchart/invitations/accept/fade6f95-af6c-4643-9f07-1e402e18cd19 following schema
 
-CREATE USER 'webAppUser'@'localhost' IDENTIFIED BY 'webAppUserPassword';
-GRANT ALL PRIVILEGES ON * . * TO 'webAppUser'@'localhost';
-FLUSH PRIVILEGES;
-
-
-
 #creates admin user with password adminpassword and gives them all permissions over all tables
 DROP USER IF EXISTS 'admin'@'localhost';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'adminpassword';
@@ -62,10 +56,10 @@ CREATE TABLE `Athlete` (
 CREATE TABLE `MedicalData` (
     medicalDataID INTEGER AUTO_INCREMENT NOT NULL,
     athleteID INTEGER NOT NULL,
-    injuries VARCHAR(100),
-    heightCM INTEGER,
-    weightKG INTEGER,
-    armSpanCM INTEGER,
+    injuries VARBINARY(255),
+    heightCM VARBINARY(255),
+    weightKG VARBINARY(255),
+    armSpanCM VARBINARY(255),
     PRIMARY KEY PKMedicalData(medicalDataID),
     FOREIGN KEY FKMedicalData(athleteID) REFERENCES Athlete(athleteID)
 );
@@ -656,7 +650,7 @@ AS
     
    
 DELIMITER //
-CREATE DEFINER='webAppUser'@'localhost' PROCEDURE user_cnt()
+CREATE DEFINER='webappuser'@'localhost' PROCEDURE user_cnt()
 SQL SECURITY INVOKER
 BEGIN
 SELECT COUNT(*) as total_users FROM mysql.user;
@@ -664,7 +658,7 @@ END;//
 DELIMITER ;
 
 DELIMITER //
-CREATE DEFINER='webAppUser'@'localhost' PROCEDURE find_number_applicants()
+CREATE DEFINER='webappuser'@'localhost' PROCEDURE find_number_applicants()
 SQL SECURITY INVOKER
 BEGIN
 SELECT COUNT(*) as total_applicants FROM athlete WHERE applicationStatus=1;
@@ -672,7 +666,7 @@ END;//
 DELIMITER ;
 
 DELIMITER //
-CREATE DEFINER='webAppUser'@'localhost' PROCEDURE find_number_coaches()
+CREATE DEFINER='webappuser'@'localhost' PROCEDURE find_number_coaches()
 SQL SECURITY INVOKER
 BEGIN
 SELECT COUNT(*) as total_coaches FROM coach;
@@ -680,14 +674,81 @@ END;//
 DELIMITER ;
 
 DELIMITER //
-CREATE DEFINER='webAppUser'@'localhost' PROCEDURE find_number_athletes()
+CREATE DEFINER='webappuser'@'localhost' PROCEDURE find_number_athletes()
 SQL SECURITY INVOKER
 BEGIN
 	SELECT COUNT(*) as total_applicants FROM athlete WHERE applicationStatus=0;
 END;//
 DELIMITER ;
 
-GRANT EXECUTE ON PROCEDURE welshrowing.find_number_athletes TO 'webAppUser'@'localhost';
-GRANT EXECUTE ON PROCEDURE welshrowing.find_number_coaches TO 'webAppUser'@'localhost';
-GRANT EXECUTE ON PROCEDURE welshrowing.find_number_applicants TO 'webAppUser'@'localhost';
-GRANT EXECUTE ON PROCEDURE welshrowing.user_cnt TO 'webAppUser'@'localhost';
+GRANT EXECUTE ON PROCEDURE welshrowing.find_number_athletes TO 'webappuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE welshrowing.find_number_coaches TO 'webappuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE welshrowing.find_number_applicants TO 'webappuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE welshrowing.user_cnt TO 'webappuser'@'localhost';
+GRANT EXECUTE ON PROCEDURE welshrowing.decrypt_data TO 'webappuser'@'localhost';
+
+#inserting dummy data
+INSERT INTO user(name,username,password,role) VALUES ("Bob Smith", "userbob", "$2a$10$3/Gbi2ytLNUsPgIoB8oeF.KllszbevLs4IxBmTbtNq48g8qD1PqDy","athlete");
+INSERT INTO user(name,username,password,role) VALUES ("James Dean", "userjames", "$2a$10$Snmk439qzaDR7XYVxGrsbesPrulnJywTOnX20VZP5cL3htcz20nRm","athlete");
+INSERT INTO user(name,username,password,role) VALUES ("Bob Ross", "painterbob", "$2a$10$AUxk0RXEIQ0wxbZjP8uFleCgTd96RNxpdgtqvQ83jEUzkxJIPVSky","athlete");
+INSERT INTO user(name,username,password,role) VALUES ("Hamid Iqbal", "hamidiqbal", "$2a$10$wIq0AGD1hxeVUm7r9pN5memAdWg37MbqQX3iuk4suMVQcsOVf0nI2","athlete");
+INSERT INTO user(name,username,password,role) VALUES ("Oliver Holdaway", "oliverholdaway", "$2a$10$2TwjPtNdaAuHj0PFXXSO5uIB2E8UIyfyh7hD29ysDyry5PlQPe2RK","athlete");
+
+INSERT INTO user(name,username,password,role) VALUES ("Coach Name", "coachaccount", "$2a$10$hczrqi5VYWFoOBxUoGmsfudaJB6ZLEDjTj5QMDAub9NsUq9X0/KQG","coach");
+INSERT INTO user(name,username,password,role) VALUES ("Nathan Baitup", "nathanbaitup", "$2a$10$aOrE8ZR.BEsL36mbnZAdLu5ZfXZdm9PykwlA29NefyWjvIOyGqzWK","coach");
+
+INSERT INTO athlete(athleteID,name,gender,DOB,applicationStatus,email,mobileNumber,telephoneNumber,address,postcode,placeOfEducation,interestLetter,postTestResult)
+VALUES (1,"Bob Smith","Male","2000-10-28",1,"bob@gmail.com","01757507758","02972503307","14 park road","PO13 5HE","Cardiff University",true,"");
+INSERT INTO athlete(athleteID,name,gender,DOB,applicationStatus,email,mobileNumber,telephoneNumber,address,postcode,placeOfEducation,interestLetter,postTestResult)
+VALUES (2,"James Dean","Male","2000-12-15",1,"james@gmail.com","01757506558","02972453307","14 green road","PO13 5HE","Cardiff University",true,"");
+INSERT INTO athlete(athleteID,name,gender,DOB,applicationStatus,email,mobileNumber,telephoneNumber,address,postcode,placeOfEducation,interestLetter,postTestResult)
+VALUES (3,"Bob Ross","Male","2000-08-15",0,"bobross@gmail.com","01753206558","02472453307","14 lake road","PO13 5HE","Cardiff University",true,"8 Weeks");
+INSERT INTO athlete(athleteID,name,gender,DOB,applicationStatus,email,mobileNumber,telephoneNumber,address,postcode,placeOfEducation,interestLetter,postTestResult)
+VALUES (4,"Hamid Iqbal","Male","2001-08-15",0,"hamid@gmail.com","01753209958","02472453907","14 red close","PO13 5HE","Cardiff University",true,"8 Weeks");
+INSERT INTO athlete(athleteID,name,gender,DOB,applicationStatus,email,mobileNumber,telephoneNumber,address,postcode,placeOfEducation,interestLetter,postTestResult)
+VALUES (5,"Oliver Holdaway","Male","2000-10-27",0,"oliver@gmail.com","01753209658","02473353907","19 red close","PO13 5HE","Cardiff University",true,"8 Weeks");
+
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-16",56,40,7,4,9,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-17",70,99,6,7,8,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-18",55,56,7,4,9,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-19",65,70,4,3,7,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-20",38,40,7,7,5,5);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-21",56,60,2,1,7,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (3,"2020-12-22",56,80,9,9,6,9);
+
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-16",67,90,7,4,9,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-17",70,99,6,7,8,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-18",33,44,7,4,9,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-19",65,70,4,3,7,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-20",38,40,7,7,5,5);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-21",60,70,2,1,7,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (4,"2020-12-22",56,80,9,9,6,9);
+
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-16",30,90,7,4,9,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-17",56,70,6,7,8,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-18",36,43,7,4,9,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-19",57,70,4,3,7,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-20",38,40,7,7,5,5);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-21",50,70,2,1,7,9);
+INSERT INTO morningmonitoring(athleteID,date,walkingHeartRate,standingHeartRate,perceivedShape,perceivedMentalState,sleepQuantityHours,sleepQuality)
+VALUES (5,"2020-12-22",66,80,9,9,6,9);
