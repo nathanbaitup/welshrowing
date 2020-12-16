bdaad21d721966035561a9ef4019544e08c36ae4
Nathan Baitup <baitupnw@cardiff.ac.uk>
2020-12-16 16:43:49 +0000

    Fixed merge conflict on AthleteDashboardController

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteDashboardController.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/AthleteDashboardController.java
@@ -34,13 +34,12 @@ public class AthleteDashboardController {
     private final AthleteAuditor athleteAuditor;
 
     @Autowired
-    public AthleteDashboardController(MorningMonitoringAuditor graphAuditor,AthleteAuditor athleteAuditor){
+    public AthleteDashboardController(MorningMonitoringAuditor graphAuditor, AthleteAuditor athleteAuditor) {
         this.graphAuditor = graphAuditor;
         this.athleteAuditor = athleteAuditor;
     }
 
     /**
-     *
      * @return
      */
     @ModelAttribute("users")
@@ -49,15 +48,15 @@ public class AthleteDashboardController {
     }
 
     @GetMapping("athlete-dashboard/{id}")
-    public String athleteDashboard(@PathVariable Long id, @ModelAttribute("users") List<Long> users, Model model){
+    public String athleteDashboard(@PathVariable Long id, @ModelAttribute("users") List<Long> users, Model model) {
         try {
             if (users.get(users.size() - 1).equals(id)) {
                 System.out.println("List of Users: " + users);
                 model.addAttribute("userID", id);
-                int[] wakingHR = {0,0,0,0,0,0,0};
-                int[] standingHR = {0,0,0,0,0,0,0};
-                int[] shape = {0,0,0,0,0,0,0};
-                String[] dates = {"","","","","","",""};
+                int[] wakingHR = {0, 0, 0, 0, 0, 0, 0};
+                int[] standingHR = {0, 0, 0, 0, 0, 0, 0};
+                int[] shape = {0, 0, 0, 0, 0, 0, 0};
+                String[] dates = {"", "", "", "", "", "", ""};
 
                 Athlete athlete = new Athlete();
 
@@ -66,41 +65,43 @@ public class AthleteDashboardController {
 
                 //checks athlete in path is present and makes athlete object
                 Optional<Athlete> optionalAthlete = athleteAuditor.findAthleteById(id);
-                if(optionalAthlete.isPresent()){
+                if (optionalAthlete.isPresent()) {
                     athlete = optionalAthlete.get();
                 }
 
                 //checks through the full list of morning monitoring entries and puts the ones for right athlete id into a different list
-                for(MorningMonitoring entry : fullList){
-                    if(entry.getAthleteID().equals(athlete)){
+                for (MorningMonitoring entry : fullList) {
+                    if (entry.getAthleteID().equals(athlete)) {
                         relevantList.add(entry);
                     }
                 }
 
                 //sets array values for the graph and checks that 7 days of records have been found.
-                if(relevantList.size()>=7) {
+                if (relevantList.size() >= 7) {
                     for (int i = 0; i < 7; i++) {
-                        int j = relevantList.size()-1;
+                        int j = relevantList.size() - 1;
 
-                        shape[6-i] = relevantList.get(j-i).getSleepQuality() +
-                                relevantList.get(j-i).getSleepQuantityHours() +
-                                relevantList.get(j-i).getPerceivedMentalState() +
-                                relevantList.get(j-i).getPerceivedShape();
+                        shape[6 - i] = relevantList.get(j - i).getSleepQuality() +
+                                relevantList.get(j - i).getSleepQuantityHours() +
+                                relevantList.get(j - i).getPerceivedMentalState() +
+                                relevantList.get(j - i).getPerceivedShape();
 
-                        wakingHR[6-i] = relevantList.get(j-i).getWalkingHeartRate();
-                        standingHR[6-i] = relevantList.get(j-i).getStandingHeartRate();
-                        dates[6-i] = relevantList.get(j-i).getDate().toString();
+                        wakingHR[6 - i] = relevantList.get(j - i).getWalkingHeartRate();
+                        standingHR[6 - i] = relevantList.get(j - i).getStandingHeartRate();
+                        dates[6 - i] = relevantList.get(j - i).getDate().toString();
                     }
                 }
 
-                AthleteMMDGraphForm graphData = new AthleteMMDGraphForm(wakingHR, standingHR, shape,dates);
-                model.addAttribute("graphs",graphData);
+                AthleteMMDGraphForm graphData = new AthleteMMDGraphForm(wakingHR, standingHR, shape, dates);
+                model.addAttribute("graphs", graphData);
 
                 return "athleteDashboard";
             } else {
                 return "redirect:/athlete-dashboard/" + +users.get(users.size() - 1);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println();
             return "redirect:/404";
         }
+    }
+}
