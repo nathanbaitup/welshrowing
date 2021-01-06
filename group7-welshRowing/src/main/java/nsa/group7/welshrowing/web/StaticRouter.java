f97432803bf25a013584eb32709f4593d7636fcd
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2021-01-06 13:10:41 +0000

    favicon.ico fixes put in place

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/StaticRouter.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/web/StaticRouter.java
@@ -1,11 +1,17 @@
 package nsa.group7.welshrowing.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticRouter implements WebMvcConfigurer {
+
+    @RequestMapping("favicon.ico")
+    String favicon() {
+        return "forward:/favicon.ico";
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
