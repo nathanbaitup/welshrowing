561172ace13de0764a9a154ea6a10d1c731d3657
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2021-01-07 18:18:51 +0000

    Configutation annontation no longer needed

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/WelshRowingProjectApplication.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/WelshRowingProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@EnableConfigurationProperties
 @SpringBootApplication
 public class WelshRowingProjectApplication {
 
