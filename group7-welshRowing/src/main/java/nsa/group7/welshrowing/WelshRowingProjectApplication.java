380f90b7d245006db820a6be3e8d3b24c3fa4779
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2021-01-07 15:23:36 +0000

    Enabled Spring Configuation

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/WelshRowingProjectApplication.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/WelshRowingProjectApplication.java
@@ -2,7 +2,10 @@ package nsa.group7.welshrowing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class WelshRowingProjectApplication {
 
