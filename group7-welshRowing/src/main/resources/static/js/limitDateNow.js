5cf27c3db17aea425e5112fec0783dda39500c66
Hamid Iqbal <iqbalha@cardiff.ac.uk>
2021-01-07 21:55:36 +0000

    Revert "Merge branch 'User-Input-Validation-FrontEnd' into 'master'"
    This reverts merge request !54

--- group7-welshRowing/src/main/resources/static/js/limitDateNow.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function() {
-var now = new Date();
-
-var day = ("0" + now.getDate()).slice(-2);
-var month = ("0" + (now.getMonth() + 1)).slice(-2);
-
-var today = now.getFullYear() + "-" + (month) + "-" + (day);
-document.getElementById("date").setAttribute("max",today);
-})
