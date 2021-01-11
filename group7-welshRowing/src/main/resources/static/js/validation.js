224c9df14227d9af7d1cdb0e90f48a0a95e79486
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-01-11 21:46:50 +0000

    Added validation for postcode and contact number.

--- group7-welshRowing/src/main/resources/static/js/validation.js
+++ group7-welshRowing/src/main/resources/static/js/validation.js
@@ -9,3 +9,25 @@ function checkNumbers(){
         return false;
     }
 }
+
+function validPhoneNumber(phoneField){
+    var patternToFollow = /^\d{11}$/;
+
+    if (phoneField.value.match(patternToFollow)) {
+        return true;
+    } else {
+        alert("Please enter a valid phone number.");
+        return false;
+    }
+}
+
+function validPostcode(postcode){
+    var pattern = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i
+
+    if (postcode.value.match(pattern)) {
+        return true;
+    } else {
+        alert("Please enter a valid postcode following LL00 0LL.");
+        return false;
+    }
+}
