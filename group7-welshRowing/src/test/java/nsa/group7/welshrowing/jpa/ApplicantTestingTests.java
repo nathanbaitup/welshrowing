7dcc0ea0265e7c2645fefd5eeb0bc471ac8f8048
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-01-16 01:36:16 +0000

    Changed test to use athlete adaptor

--- group7-welshRowing/src/test/java/nsa/group7/welshrowing/jpa/ApplicantTestingTests.java
+++ group7-welshRowing/src/test/java/nsa/group7/welshrowing/jpa/ApplicantTestingTests.java
@@ -10,13 +10,14 @@ import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-prod.properties")
 public class ApplicantTestingTests {
 
     @Autowired
-    private AthleteRepoJPA athleteRepoJPA;
+    private AthleteJPAAdaptor athleteJPAAdaptor;
 
     @Autowired
     private ApplicantTestingRepo applicantTestingRepo;
@@ -24,24 +25,24 @@ public class ApplicantTestingTests {
 
     @Test
     public void shouldSubmitApplicantTest() throws Exception {
-        Athlete athlete = athleteRepoJPA.findByAthleteID(1L);
+        Athlete athlete = athleteJPAAdaptor.findAthleteById(1L).get();
         applicantTestingRepo.save(new ApplicantTesting(1L, athlete, "2020-12-09", "should equal this", 1, 1, 1, 1, 1, "Gave Up", "Amber", "", "Amber", ""));
         assertEquals("should equal this", applicantTestingRepo.findByAthleteTestID(1L).getAthleteComments());
     }
 
     @Test
     public void shouldUpdatePostTestAndApplicationStatus() throws Exception {
-        Long athleteID = athleteRepoJPA.findByAthleteID(1L).getAthleteID();
+        Long athleteID = athleteJPAAdaptor.findAthleteById(1L).get().getAthleteID();
 
         //Testing that an applicationStatus can be updated.
-        athleteRepoJPA.setApplicationStatus(Boolean.TRUE, athleteID); // sets applicationStatus to true before testing to ensure test sees change.
-        athleteRepoJPA.setApplicationStatus(Boolean.FALSE, athleteID );
-        assertEquals(false, athleteRepoJPA.findByAthleteID(1L).getApplicationStatus());
+        athleteJPAAdaptor.setApplicationStatus(Boolean.TRUE, athleteID); // sets applicationStatus to true before testing to ensure test sees change.
+        athleteJPAAdaptor.setApplicationStatus(Boolean.FALSE, athleteID );
+        assertEquals(false, athleteJPAAdaptor.findAthleteById(1L).get().getApplicationStatus());
 
         //Testing that a postTestResult can be updated.
-        athleteRepoJPA.setPostTestStatus("", athleteID); // sets postTestResult to blank before testing to ensure test sees change.
-        athleteRepoJPA.setPostTestStatus("START.", athleteID);
-        assertEquals("START.", athleteRepoJPA.findByAthleteID(1L).getPostTestResult());
+        athleteJPAAdaptor.setPostTestStatus("", athleteID); // sets postTestResult to blank before testing to ensure test sees change.
+        athleteJPAAdaptor.setPostTestStatus("START.", athleteID);
+        assertEquals("START.", athleteJPAAdaptor.findAthleteById(1L).get().getPostTestResult());
     }
 
 
