ca3a268b41cb3afca440305339b807b8631c0746
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-03-02 21:27:55 +0000

    Changed user table name to User instead of user.

--- group7-welshRowing/src/main/java/nsa/group7/welshrowing/domain/Applicant.java
+++ group7-welshRowing/src/main/java/nsa/group7/welshrowing/domain/Applicant.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "User")
 public class Applicant {
     /**
      * The ID that will be assigned to a user.
